b436faefbab6066fb108425ddb501be1e92881b7
C57B6J <mrgilberto@gmail.com>
2025-04-14 15:59:54 -0400

    Update NIH_query.py

--- NIH_query.py
+++ NIH_query.py
@@ -6,7 +6,7 @@ endpoint = "https://api.reporter.nih.gov/v2/projects/search"
 
  # query_payload = {
  #     "criteria": {
- #         "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": 
+ #         "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascular\" OR \"basilar membrane\" OR "\reissner membrane\" OR \"inner hair cell"\ OR \"outer hair cell"\ OR \"auditory hair cell"\ OR \"spiral gangli"\ OR \"auditory nerv"\ OR \"vestibular gangli\" OR \"vestibular nerv"\ OR \"vestibular epitheli\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR otolith OR otoconi \"crista ampullaris\" OR \" utric"\ OR saccul NOT \"prostatic\" NOT \"pharyngeal\")"}
  #     },
  #     "include_fields": [
  #             "FiscalYear", "ProjectNum"
@@ -21,7 +21,7 @@ endpoint = "https://api.reporter.nih.gov/v2/projects/search"
 
 query_payload = {
     "criteria": {
-            "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascularis\" OR \"reissner membrane\" OR \"spiral ganglion\" OR \"basilar membrane\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR \"otolith\" OR \"crista ampullaris\" OR  \"utricle\" NOT \"prostatic\" NOT \"pharyngeal\")"}
+            "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascular\" OR \"basilar membrane\" OR "\reissner membrane\" OR \"inner hair cell"\ OR \"outer hair cell"\ OR \"auditory hair cell"\ OR \"spiral gangli"\ OR \"auditory nerv"\ OR \"vestibular gangli\" OR \"vestibular nerv"\ OR \"vestibular epitheli\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR otolith OR otoconi \"crista ampullaris\" OR \" utric"\ OR saccul NOT \"prostatic\" NOT \"pharyngeal\")"}
     },
     "include_fields": [
             "FiscalYear", "ProjectNum", "ActivityCode","AwardType","Organization"
