60ea8f69a5a6a1efe525433691f8bd987e06e61c
C57B6J <mrgilberto@gmail.com>
2025-04-14 15:55:57 -0400

    Update NIHReporter_JSON_print.py

--- NIHReporter_JSON_print.py
+++ NIHReporter_JSON_print.py
@@ -5,7 +5,7 @@ endpoint = "https://api.reporter.nih.gov/v2/projects/search"
 query_payload = {
         "criteria": {
             "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": 
-                                     "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascular\" OR \"basilar membrane\" OR "\reissner membrane\" OR \"inner hair cell"\ OR \"outer hair cell"\ OR \"auditory hair cell"\ OR \"spiral gangli"\ OR \"auditory nerv"\ OR \"vestibular gangli\" OR \"vestibular nerv"\ OR \"vestibular epitheli\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR otolith OR otoconi \"crista ampullaris\" OR \" utric"\ OR saccul NOT \"prostatic\" NOT \"pharyngeal\")"}
+                "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascular\" OR \"basilar membrane\" OR "\reissner membrane\" OR \"inner hair cell"\ OR \"outer hair cell"\ OR \"auditory hair cell"\ OR \"spiral gangli"\ OR \"auditory nerv"\ OR \"vestibular gangli\" OR \"vestibular nerv"\ OR \"vestibular epitheli\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR otolith OR otoconi \"crista ampullaris\" OR \" utric"\ OR saccul NOT \"prostatic\" NOT \"pharyngeal\")"}
             },
         "include_fields": [
             "FiscalYear", "ProjectNum", "ActivityCode","AwardType","Organization", "AwardAmount", "AbstractText"
