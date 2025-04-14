353924fc30b6f19852a05615a0146afb9af11e6b
C57B6J <mrgilberto@gmail.com>
2025-04-14 15:58:34 -0400

    Update NIH_query.py

--- NIH_query.py
+++ NIH_query.py
@@ -6,7 +6,7 @@ endpoint = "https://api.reporter.nih.gov/v2/projects/search"
 
  # query_payload = {
  #     "criteria": {
- #         "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": "(cochlea OR \"basilar papilla\" OR \"organ of corti\" OR \"stria vascularis\" OR \"basilar membrane\" OR \"vestibular labyrinth\" OR \"semicircular canal\" OR \"vestibular macula\" OR otolith OR crista ampullaris OR utricle)"}
+ #         "advanced_text_search": { "search_field": "terms,projecttitle", "search_text": 
  #     },
  #     "include_fields": [
  #             "FiscalYear", "ProjectNum"
